9bf060efb98dfcb3a90da9c13a64beca126e374f
Osiris de Castro <112344929+osirissn@users.noreply.github.com>
2023-07-10 22:43:24 -0300

    Create No.java

---
+++ No.java
@@ -0,0 +1,20 @@
+public No(Object element, No prox) {
+    this.element = element;
+    this.prox = prox;
+}
+
+public Object getElement() {
+    return element;
+}
+
+public void setElement(Object element) {
+    this.element = element;
+}
+
+public No getProx() {
+    return prox;
+}
+
+public void setProx(No prox) {
+    this.prox = prox;
+}
